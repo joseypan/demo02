33d1d494a98d34e7764c31e9c0674523a16e8191
joseypan <joseypan@163.com>
2021-01-20 09:05:29 +0800

    引入oss

--- src/renderer/views/File.vue
+++ src/renderer/views/File.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <el-button @click="watchFileChange">监控文件变化</el-button>
+    <el-button @click="ossUpload">上传文件</el-button>
   </div>
 </template>
 <script>
@@ -23,6 +24,7 @@ export default {
       const watcher = chokidar.watch(targetPath, {
         ignoreInitial: true,
         persistent: true,
+        depth: 2,
         awaitWriteFinish: {
           stabilityThreshold: 2000,
           pollInterval: 100
@@ -36,6 +38,29 @@ export default {
         .on('add', path => log(`File ${path} has been added`))
         .on('change', path => log(`File ${path} has been changed`))
         .on('unlink', path => log(`File ${path} has been removed`))
+    },
+    async ossUpload () {
+      const { dialog } = this.$electron.remote
+      let targetPath = dialog.showOpenDialog({
+        // 可以设置默认的文件名
+        properties: ['openFile']
+      })
+      const Oss = require('ali-oss')
+      let client = new Oss({
+        region: 'oss-cn-shanghai',
+        accessKeyId: 'LTAI4G7DtuvJNsx3gJLaMn4p',
+        accessKeySecret: 'frV9qxb9UAxXzZA6i7Ie6uErAovWzL',
+        bucket: 'josey'
+      })
+      try {
+        // 'object'表示上传到OSS的object名称，'localfile'表示本地文件或者文件路径。
+        let r1 = await client.put('uploadFile', targetPath[0])
+        console.log('put success: %j', r1)
+        // let r2 = await client.get('object')
+        // console.log('get success: %j', r2)
+      } catch (e) {
+        console.error('error: %j', e)
+      }
     }
   }
 }
